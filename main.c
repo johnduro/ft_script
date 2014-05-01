3a2dc46b644cbb2c3d6c0922f8449063f482ea82
johnduro <https://johnduro@bitbucket.org>
2014-05-01 04:02:40 +0200

    clarification du code et ajout de commentaires

--- main.c
+++ main.c
@@ -32,40 +32,45 @@ int main()
 	name = ft_strnew(0);
 	if ((file = open("test_1", O_RDWR | O_CREAT | O_TRUNC, 0666)) == -1)
 		exit(printf("exit fail"));
-    if (openpty(&fdm, &fds, name, NULL, NULL))
+    if (openpty(&fdm, &fds, name, NULL, NULL)) //a refaire
       exit(1);
 	printf("Script started, output file is %s\n", name);
     if ((pid = fork()) < 0)
       exit(2);
-    if (!pid) {
+    if (!pid)
+	{
       // child
-      close(fdm);
-      login_tty(fds);
-      execl("/bin/zsh", "zsh", NULL);
-      exit(3);
+    	close(fdm);
+    	login_tty(fds); //a refaire
+    	execl("/bin/zsh", "zsh", NULL);
+    	exit(3);
     }
     /* father: copy stdin/out to/from master */
-    close(fds); system("stty raw -echo");
+    close(fds);
+	system("stty raw -echo"); //a refaire man 3 -> surement des modification des termcaps void man
     FD_ZERO(&set);
-    while (waitpid(pid, &i, WNOHANG)!=pid) {
-      FD_SET(0, &set);
-      FD_SET(fdm, &set);
-      select(fdm+1, &set, NULL, NULL, NULL);
-      if (FD_ISSET(0, &set)) {
-          i = read(0, buf, 64);
-          if (i>0)
-			  write(fdm, buf, i);
-      }
-      if (FD_ISSET(fdm, &set)) {
-          i = read(fdm, buf, 64);
-          if (i>0)
-		  {
-			  write(1, buf, i);
-			  write(file, buf, i);
-		  }
-      }
+    while (waitpid(pid, &i, WNOHANG)!=pid)
+	{
+    	FD_SET(0, &set);
+    	FD_SET(fdm, &set);
+    	select(fdm+1, &set, NULL, NULL, NULL);
+    	if (FD_ISSET(0, &set))
+		{
+			i = read(0, buf, 64);
+        	if (i>0)
+				write(fdm, buf, i);
+      	}
+   		if (FD_ISSET(fdm, &set))
+		{
+      		i = read(fdm, buf, 64);
+       		if (i>0)
+			{
+				write(1, buf, i);
+				write(file, buf, i);
+		  	}
+      	}
     }
 	printf("\nScript done, output file is %s", name);
-    system("stty sane");
+    system("stty sane"); //a refaire man 3 surement termcaps
     exit(0);
 }
